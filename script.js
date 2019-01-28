97aa194b259dadc6a46e393de476d88e5b83e3c7
Verkir <alec.m.metcalfe@gmail.com>
2019-01-28 19:07:28 -0400

    Added hotkey for sidebar open

---
+++ script.js
@@ -0,0 +1,36 @@
+/* 
+    Navbar function 
+    Example code taken and modified from Bootstrapious
+*/
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('.overlay').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+
+/*
+    Date picker function
+    Example code taken and modified from Bootstrapious
+*/
+$(document).ready(function(){
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    date_input.datepicker({
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    })
+})
