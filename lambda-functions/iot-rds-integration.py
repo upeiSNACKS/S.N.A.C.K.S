2827d05266585ea314f6d2c35c101ef801eaffb0
mregger <eduardoegger@gmail.com>
2019-02-09 14:54:05 -0400

    added a new folder for the two lambda functions, one script for inbound and one script for outbound data

---
+++ lambda-functions/iot-rds-integration.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+#by Eduardo"
+
+'''
+The following script is meant to be used in an AWS Lambda environment, using
+python 3.6. To run, it requires pymysql, and the database access keys set
+as environment parameters.
+'''
+
+import pymysql
