41dbcfb7cc574e6775cb31c43ffe30d74b4f7a16
rjarsenault1101 <rjarsenault1101@gmail.com>
2019-02-09 20:34:36 -0400

    updated SQL scripts

--- database scripts/Create_main_tables.sql
+++ database scripts/Create_main_tables.sql
@@ -4,9 +4,10 @@ CREATE TABLE Owners (
     lname varchar(64), 
     address varchar(128));
 CREATE TABLE Types (
-	type varchar(32) not null primary key, 
-    subtype varchar(32) default '', 
-    unit varchar(8) default '');
+	sensor_type varchar(32) not null, 
+    sensor_subtype varchar(32) default '', 
+    unit varchar(8) default '',
+    CONSTRAINT PRIMARY KEY (sensor_type, sensor_subtype));
 CREATE TABLE Sensors ( 
 	sensor_id varchar(64) not null,
     owner_email varchar(128) default '', 
@@ -17,12 +18,12 @@ CREATE TABLE Sensors (
 		ON DELETE NO ACTION ON UPDATE CASCADE );
 CREATE TABLE Readings (
 	sensor_id varchar(64) not null, 
-    sensor_type varchar(32) not null, 
+    sensor_type varchar(32) default '', 
     sensor_subtype varchar(32) not null, 
-    time TIMESTAMP default CURRENT_TIMESTAMP, 
+    reading_time TIMESTAMP default CURRENT_TIMESTAMP, 
     reading varchar(16),
-    CONSTRAINT PRIMARY KEY (sensor_id, sensor_type, sensor_subtype, time), 
+    CONSTRAINT PRIMARY KEY (sensor_id, sensor_type, sensor_subtype, reading_time), 
     CONSTRAINT FOREIGN KEY (sensor_id) references Sensors (sensor_id)
 		ON UPDATE CASCADE ON DELETE NO ACTION, 
-	CONSTRAINT FOREIGN KEY (sensor_type, sensor_subtype) references Types (type, subtype)
+	CONSTRAINT FOREIGN KEY (sensor_type, sensor_subtype) references Types (sensor_type, sensor_subtype)
 		ON UPDATE CASCADE ON DELETE NO ACTION);
--- database scripts/create_sensormetrics_typemetrics_views.sql
+++ database scripts/create_sensormetrics_typemetrics_views.sql
@@ -4,30 +4,34 @@ CREATE VIEW Sensor_metrics AS
 			sensor_type, 
             sensor_subtype, 
             count(*) as count, 
-            min(sensor_reading) as minimum, 
-            max(sensor_reading) as maximum, 
-            avg(sensor_reading) as average
+            min(reading) as minimum, 
+            max(reading) as maximum, 
+            avg(reading) as average
             from Readings group by sensor_id;
+CREATE VIEW Max_values as 
+select sensor_type, 
+					sensor_subtype, 
+					max(reading) as max 
+                    from Readings group by sensor_type, sensor_subtype;
+CREATE VIEW Min_values as
+(select sensor_type, 
+					sensor_subtype, 
+                    min(reading) as min 
+                    from Readings group by sensor_type, sensor_subtype);
 CREATE VIEW Type_metrics AS 
 	SELECT  sensor_type, 
 			sensor_subtype, 
-            max(sensor_reading) as maximum, 
-            (select a.sensor_time 
+            max(reading) as maximum, 
+            (select a.reading_time 
             from Readings a inner join 
-			(select sensor_type, 
-					sensor_subtype, 
-					max(sensor_reading) as max 
-                    from Readings group by sensor_type, sensor_subtype) b 
+			Max_values b 
 				on a.sensor_type = b.sensor_type and a.sensor_subtype = b.sensor_subtype 
-				and a.sensor_reading = b.max limit 1) as max_time,
-            min(sensor_reading) as minumum, 
-            (select a.sensor_time 
+				and a.reading = b.max limit 1) as max_time,
+            min(reading) as minumum, 
+            (select a.reading_time 
             from Readings a inner join 
-			(select sensor_type, 
-					sensor_subtype, 
-                    min(sensor_reading) as max 
-                    from Readings group by sensor_type, sensor_subtype) b 
+			Min_values b 
 				on a.sensor_type = b.sensor_type and a.sensor_subtype = b.sensor_subtype 
-				and a.sensor_reading = b.max limit 1) as min_time,
-            avg(sensor_reading) as average
+				and a.reading = b.min limit 1) as min_time,
+            avg(reading) as average
             from Readings group by sensor_type, sensor_subtype;
