47a9636926f0f5ab8bf8b879f7470705557a6ee6
Jeremy Thompson <jeremythompson.918@gmail.com>
2019-02-18 22:53:45 -0400

    Leave zoom control in default position

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     */
     var mymap = L.map('mapid').setView([46.2512, -63.1350], 13);
 
-    mymap.zoomControl.setPosition('topright');
+    // mymap.zoomControl.setPosition('topright');
 
 	  // adding custom menu button
     var menuControl = L.Control.extend({
