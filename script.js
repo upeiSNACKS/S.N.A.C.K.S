5ce570c03a476d5de686088720ab7f34f1d84f73
Jeremy Thompson <jeremythompson.918@gmail.com>
2019-02-18 22:12:46 -0400

    Add comment

--- script.js
+++ script.js
@@ -101,6 +101,7 @@ $(document).ready(function () {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mymap);
 
+    /* add marker on right click */
     mymap.on("contextmenu", function (event) {
         console.log("user right-clicked on map coordinates: " + event.latlng.toString());
         L.marker(event.latlng).addTo(mymap);
