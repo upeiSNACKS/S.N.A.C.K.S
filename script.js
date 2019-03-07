9fce31a208800bc4034cd503261cd806c3f631a1
Alec <almetcalfe@upei.ca>
2019-03-07 11:47:12 -0400

    Removed unneeded lines

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     /*
         Navbar
@@ -28,10 +27,7 @@ $(document).ready(function () {
     // limit zoom level since Charlottetown is not that large
     mymap.options.minZoom = 12;
 
-
-    // mymap.zoomControl.setPosition('topright');
-
-	  // adding custom menu button
+    // adding custom menu button
     var menuControl = L.Control.extend({
 
         options: {
@@ -190,7 +186,7 @@ $(function() {
         httpc.open("POST", url, true); // sending as POST
 
         httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        //httpc.setRequestHeader("Content-Length", params.length); // POST request MUST have a Content-Length header (as per HTTP/1.1)
+        // POST request MUST have a Content-Length header (as per HTTP/1.1)
 
         httpc.onreadystatechange = function() { //Call a function when the state changes.
             if(httpc.readyState == 4 && httpc.status == 200) { // complete and no errors
