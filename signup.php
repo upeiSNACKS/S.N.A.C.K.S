bfbc8203bb8c2b699d2a288e9c0d5d44aae3f385
rjarsenault1101 <rjarsenault1101@gmail.com>
2019-03-19 20:59:01 -0300

    additional features added

--- signup.php
+++ signup.php
@@ -1,15 +1,23 @@
 <?php
     require_once 'loginPending.php';
     echo "Successfully submitted form. Waiting for approval from an Administrator. Thanks :-) ";
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $address = $_POST['address'];
-    $sensor_id = $_POST['sensorID'];
-    $sensor_lat = $_POST['sensor_lat'];
-    $sensor_lon = $_POST['sensor_lon'];
-
 
     $connection = new mysqli($db_hostname, $db_username, $db_password, $db_database);
     if($connection->connect_error) die($connection->connect_error);
+    $fname = mysqli_real_escape_string($connection, $_POST['fname']);
+    $lname = mysqli_real_escape_string($connection, $_POST['lname']);
+    $email = mysqli_real_escape_string($connection, $_POST['email']);
+    $address = mysqli_real_escape_string($connection, $_POST['address']);
+    $sensor_id = mysqli_real_escape_string($connection, $_POST['sensorID']);
+    $sensor_lat = mysqli_real_escape_string($connection, $_POST['sensor_lat']);
+    $sensor_lon = mysqli_real_escape_string($connection, $_POST['sensor_lon']);
+    $query = "INSERT INTO Pending_insert SET fname='$fname', lname='$lname', email='$email', address='$address', sensor_id='$sensor_id', sensor_lat='$sensor_lat', sensor_lon='$sensor_lon'";
+    $result = $connection->query($query);
+    if(!$result) echo "INSERT INTO failed: $types_q<br>" . $connection->error . "<br><br>";
+    $result = $connection->query("SELECT LAST_INSERT_ID()");
+    if(!$result) echo "SELECT  failed: $types_q<br>" . $connection->error . "<br><br>";
+    $insertID = $result->fetch_array(MYSQLI_NUM)[0];
+    echo "$insertID <br><br>";
+
+
  ?>
