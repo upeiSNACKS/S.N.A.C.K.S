cfdc2478c338ca28c43fa1c446fa9cebc498e155
rjarsenault1101 <rjarsenault1101@gmail.com>
2019-03-19 13:56:47 -0300

    added stuff again

--- validation.js
+++ validation.js
@@ -1,59 +1,52 @@
-function validate(form){
-    console.log("VALIDATED");
-    fail = validateForename(form.fname.value)
-    fail += validateSurname(form.lname.value)
-    fail += validateEmail(form.email.value)
-    fail += validateSensorID(form.sensorID.value)
-    fail += validateLat(form.sensor_lat.value)
-    fail += validateLon(form.sensor_lon.value)
-    fail += validatecheck(form.dataconsent.checked)
-    if (fail == "") {
-        swal("Success!", "you did it!", "success");
-        return true;
-    } else {
-        swal("Error", fail, "error");
-        return false;
-     }
-}
-function validatecheck(field) {
-    if(!field) {
-        return "You must consent to the data being publicly available";
-    } else {
-        return "";
+$(document).ready(function() {
+    /**
+     * This function uses AJAX to populate a JSON array which gets used by our leaflet map
+     */
+     var usedIDs;
+     ajax("");
+    function ajax(params) {
+        // From StackOverflow: https://stackoverflow.com/questions/406316/how-to-pass-data-from-javascript-to-php-and-vice-versa
+        var httpc = new XMLHttpRequest(); // simplified for clarity
+        httpc.withCredentials = false;
+
+        var url = "validation.php";
+        httpc.open("POST", url, true);
+        httpc.setRequestHeader("Content-Type", "application/json");
+        httpc.onreadystatechange = function() { //Call a function when the state changes.
+            if(httpc.readyState == 4 && httpc.status == 200) { // complete and no errors
+                usedIDs = JSON.parse(httpc.responseText);
+            }
+        };
+        httpc.send();
     }
-}
-function validateForename(field){
-    return (field == "") ? "No first name was entered.\n" : ""
-}
-function validateSurname(field){
-    return (field == "") ? "No last name was entered.\n" : ""
-}
-function validateSensorID(field){
-    if (field == "")
-        return "No sensor ID  was entered.\n"
-    else if (/[^a-zA-Z0-9_-]/.test(field))
-        return "Only a-z, A-Z, 0-9, - and _ allowed in sensor ID.\n"
-    return ""
-}
-function validateEmail(field)
-{
-    if (field == "")
-        return "No Email was entered.\n"
-    else if (!((field.indexOf(".") > 0) && (field.indexOf("@") > 0)) || /[^a-zA-Z0-9.@_-]/.test(field))
-        return "The Email address is invalid.\n"
-    return ""
-}
-function validateLat(field) {
-    if(field >= 46.2 && field <= 46.3) {
-        return "";
-    } else {
-        return "Latitude is not within Charlottetown bounds\n"
-    }
-}
-function validateLon(field) {
-    if(field >= -63.1 && field <= -63.0) {
-        return "";
-    } else {
-        return "Longitute is not within Charlottetown bounds\n"
-    }
-}
+    var failMessage = "";
+    $('#sensorID').on('blur', function() {
+        var contents = $('#sensorID').val();
+        var valid = true;
+        for(var i = 0;i<usedIDs.length && valid; i++) {
+            if(contents == usedIDs[i]) {
+                failMessage += "Invalid sensor ID - Already in use\n";
+                valid = false;
+            }
+        }
+    });
+
+    $('#sensor_lat').on('blur', function() {
+        var contents = $('#sensor_lat').val();
+        if(contents < 46.227094 || contents > 46.306999) {
+            failMessage += "Latitude is outside of Charlottetown city bounds\n";
+        }
+    });
+    $('#sensor_lon').on('blur', function() {
+        var contents = $('#sensor_lon').val();
+        if(contents < -63.140189 || contents > -63.092608) {
+            failMessage += "Longitude is outside of Charlottetown city bounds\n";
+        }
+    });
+    $('#insertForm').submit(function() {
+        if(failMessage === "") {
+            swal("Error", failMessage, "error");
+        }
+    })
+
+});
