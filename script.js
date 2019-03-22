617536afbc97be95d872e6356ea65d9a27ca017d
rjarsenault1101 <rjarsenault1101@gmail.com>
2019-03-22 17:13:54 -0300

    forgot to save lol

--- script.js
+++ script.js
@@ -1,284 +1,3 @@
-<<<<<<< HEAD
-var grapes_small = L.icon({
-    iconUrl: 'map-icon.png',
-    iconSize:     [20, 20], // size of the icon
-    iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-});
-
-var grapes_medium = L.icon({
-    iconUrl: 'map-icon.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-});
-
-var grapes_large = L.icon({
-    iconUrl: 'map-icon.png',
-    iconSize:     [60, 60], // size of the icon
-    iconAnchor:   [30, 30], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-});
-$(document).ready(function () {
-    /*
-        Navbar
-        Example code taken and modified from Bootstrapious
-    */
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-
-    $('#dismiss, .overlay').on('click', function () {
-        $('#sidebar').removeClass('active');
-        $('.overlay').removeClass('active');
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $('.overlay').toggleClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-
-    $('#help').on('click', function () {
-       window.open('help.html', '_blank');
-    });
-
-    /*
-        Map
-    */
-    var mymap = L.map('mapid').setView([46.2512, -63.1350], 13);
-
-    // limit zoom level since Charlottetown is not that large
-    mymap.options.minZoom = 12;
-
-    // sometimes bounce will break grouping fnctionality - so disable it
-    mymap.options.bounceAtZoomLimits = false;
-
-    var timeControl = L.Control.extend({
-
-        options: {
-            position: 'topright'
-            //control position - allowed: 'topleft', 'topright', 'bottomleft', 'bottomright'
-        },
-
-        onAdd: function (map) {
-            var navigation = L.DomUtil.create('nav');
-            var container = L.DomUtil.create('div', 'timecontrol', navigation);
-            var timepicker = L.DomUtil.create('input', '', container);
-            timepicker.name = 'datetimes';
-            timepicker.id = 'timepicker';
-            timepicker.type = 'input';
-            timepicker.accessKey = 't';
-
-            return container;
-        },
-
-    });
-
-    mymap.addControl(new timeControl());
-
-    L.Control.Watermark = L.Control.extend({
-        onAdd: function(map) {
-            var img = L.DomUtil.create('img');
-
-            img.src = 'Charlottetown_Logo.png';
-            img.style.width = '200px';
-
-            return img;
-        }
-    });
-
-    L.control.watermark = function(opts) {
-        return new L.Control.Watermark(opts);
-    }
-
-    L.control.watermark({ position: 'bottomleft'}).addTo(mymap);
-
-    // creating custom differently sized icons
-
-
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(mymap);
-    setMap(mymap);
-    // disable clustering once zoomed in close enough
-    var markers = L.markerClusterGroup({ disableClusteringAtZoom: 15 });
-    setLayer(markers);
-    ajax("?start_time=now");
-
-    //attempting resizing of all markers based on zoom levels
-    // highest is level 18, when zoomed all the way in
-    // lowest is level 0, where you can see entire world repeated multiple times
-    // TODO: determine if this is necessary or how to resize on zoom levels
-    mymap.on('zoomend', function() {
-        var currentZoom = mymap.getZoom();
-        if (currentZoom > 12) {
-            //all_sensors.eachLayer(function(layer) {
-                //return layer.setIcon(fontAwesomeIcon);
-            //});
-        } else {
-            //all_sensors.eachLayer(function(layer) {
-                //return layer.setIcon(fontAwesomeIcon);
-            //});
-        }
-    });
-});
-var globalMap;
-var layer;
-function setLayer(l) {
-    layer = l;
-}
-function getLayer() {
-    return layer;
-}
-function setMap(map) {
-    globalMap = map;
-}
-function getMap() {
-    return globalMap;
-}
-/*
- * creates the popup you see when a pin is clicked. Ran for each pin.
- */
-function constructPopupHTML(feature) {
-    var table = document.createElement("table");
-
-    // Add table header containing the last reading time
-    var tr = table.insertRow(-1);
-    var th = document.createElement("th");
-    th.innerHTML = "Last measurement"
-    tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = feature.properties.reading_time;
-    tr.appendChild(th);
-
-    for (var i = 0; i < feature.properties.readings.length; i++) {
-        //console.log(feature.properties.readings[i]);
-        tr = table.insertRow(-1);
-        var tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = feature.properties.readings[i].type + ", " + feature.properties.readings[i].subtype;
-        tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = feature.properties.readings[i].reading;
-    }
-
-    var divContainer = document.getElementById("popup_template");
-    divContainer.innerHTML = "";
-    divContainer.appendChild(createHeader(feature.properties.name));
-    divContainer.appendChild(table);
-    return $("#popup_template").html();
-}
-/*
- * Creates the header for the popupHTML (usually the sensor name)
- */
-function createHeader(name) {
-    var header = document.createElement("h2");
-    header.innerHTML = name;
-    return header
-}
-/**
- * This function uses AJAX to populate a JSON array which gets used by our leaflet map
- */
-function ajax(params) {
-    // From StackOverflow: https://stackoverflow.com/questions/406316/how-to-pass-data-from-javascript-to-php-and-vice-versa
-    var httpc = new XMLHttpRequest(); // simplified for clarity
-    httpc.withCredentials = false;
-    if (params.indexOf("?") != 0) {
-        params = "?" + params;
-    }
-    var url = "https://jm6ctx1smj.execute-api.us-east-2.amazonaws.com/beta/DBapiAccess" + params;
-    httpc.open("GET", url, true);
-    console.log(url);
-    httpc.setRequestHeader("Content-Type", "application/json");
-    httpc.onreadystatechange = function() { //Call a function when the state changes.
-        if(httpc.readyState == 4 && httpc.status == 200) { // complete and no errors
-            var receivedJSON = JSON.parse(httpc.responseText);
-            var modifiedJSON = [];
-            for(var i = 0; i<receivedJSON.length; i++) {
-                // If we don't have this SensorID already in our GEOJSON, we create a new GEOJSON object for it
-                if(!checkThere(modifiedJSON, receivedJSON[i])) {
-                    // This is how GEOJSON is supposed to look. The stuff in properties is optional, but helps us.
-                    var newObj = {  "type": "Feature",
-                                    "properties": {
-                                        "name": receivedJSON[i].sensor_id,
-                                        "reading_time" : receivedJSON[i].reading_time,
-                                        // An array of readings, to hold every reading over the time period that we received
-                                        "readings": [
-                                            {"type": receivedJSON[i].sensor_type,
-                                             "subtype": receivedJSON[i].sensor_subtype,
-                                             "reading": receivedJSON[i].reading}
-                                        ]},
-                                    "geometry": {
-                                        "type": "Point",
-                                        "coordinates": [
-                                            receivedJSON[i].sensor_lon,
-                                            receivedJSON[i].sensor_lat
-                                        ]
-                                    }
-                                };
-                    modifiedJSON.push(newObj)
-                }
-            }
-            sensors=modifiedJSON;
-            var map = getMap();
-            var all_sensors = L.geoJSON(sensors, {
-              onEachFeature: function (feature, layer) {
-                layer.setIcon(grapes_medium);
-                layer.bindPopup(
-                  constructPopupHTML(feature)
-                );
-              }
-            });
-            var markers = L.markerClusterGroup({ disableClusteringAtZoom: 15 });
-            map.removeLayer(getLayer());
-            markers.addLayer(all_sensors);
-
-            map.addLayer(markers);
-        }
-    };
-    httpc.send();
-}
-/*
- * This function checks our list of GEOJSON objects to see if the sensorID already
- * has an element. If it does we take the reading and put it in the sensorID's object
- */
-function checkThere(list, obj) {
-    for(var i = 0; i<list.length; i++) {
-        if(obj.sensor_id == list[i].properties.name) {
-            list[i].properties.readings.push({
-                "type": obj.sensor_type,
-                "subtype": obj.sensor_subtype,
-                "reading": obj.reading});
-            return true;
-        }
-    }
-    return false;
-}
-/*
-    Date picker
-    Example code taken from daterangepicker.com
-*/
-$(function() {
-    $('input[name="datetimes"]').daterangepicker({
-        timePicker: true,
-        startDate: moment().startOf('hour'),
-        endDate: moment().startOf('hour').add(32, 'hour'),
-        applyButtonClasses: 'apply',
-        cancelButtonClasses: 'cancel',
-        locale: {
-            format: 'M/DD hh:mm A'
-        }
-    });
-    $('input[name="datetimes"]').on('apply.daterangepicker', function(ev, picker) {
-        var start = picker.startDate.format('YYYY-MM-DD hh:mm');
-        var end   = picker.endDate.format('YYYY-MM-DD hh:mm');
-        var params = "?start_time=" + start + "&end_time=" + end;
-        ajax(params);
-    });
-});
-=======
 var grapes_small = L.icon({
     iconUrl: 'map-icon.png',
     iconSize:     [20, 20], // size of the icon
@@ -382,7 +101,7 @@ $(document).ready(function () {
         maxZoom: 18,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mymap);
-    
+
     setMap(mymap);
     // disable clustering once zoomed in close enough
     var markers = L.markerClusterGroup({ disableClusteringAtZoom: 15 });
@@ -494,7 +213,7 @@ function calcAverage(json, type) {
             }
         }
     }
-    
+
     return avg/counter;
 }
 
@@ -505,11 +224,11 @@ function ajax(params) {
     // From StackOverflow: https://stackoverflow.com/questions/406316/how-to-pass-data-from-javascript-to-php-and-vice-versa
     var httpc = new XMLHttpRequest(); // simplified for clarity
     httpc.withCredentials = false;
-    
+
     if (params.indexOf("?") != 0) {
         params = "?" + params;
     }
-    
+
     var url = "https://jm6ctx1smj.execute-api.us-east-2.amazonaws.com/beta/DBapiAccess" + params;
     httpc.open("GET", url, true);
     console.log(url);
@@ -522,7 +241,7 @@ function ajax(params) {
             for(var i = 0; i < receivedJSON.length; i++) {
                 // If we don't have this SensorID already in our GEOJSON, we create a new GEOJSON object for it
                 if(!checkThere(modifiedJSON, receivedJSON[i])) {
-                    var newObj = {  
+                    var newObj = {
                         "type": "Feature",
                         "properties": {
                             "name": receivedJSON[i].sensor_id,
@@ -544,16 +263,16 @@ function ajax(params) {
                             ]
                         }
                     };
-                    
+
                     modifiedJSON.push(newObj);
                 }
             }
-            
+
             document.getElementById("num_sensors").innerHTML = modifiedJSON.length;
             document.getElementById("last_reading").innerHTML = modifiedJSON[0].properties.reading_time;
             document.getElementById("temp_avg").innerHTML = "Average: " + calcAverage(modifiedJSON, "Temperature") + "&deg;C";
             document.getElementById("hum_avg").innerHTML = "Average: " + calcAverage(modifiedJSON, "Humidity") + "%";
-            
+
             sensors = modifiedJSON;
             var map = getMap();
             var all_sensors = L.geoJSON(sensors, {
@@ -564,7 +283,7 @@ function ajax(params) {
                     );
                 }
             });
-            
+
             var markers = L.markerClusterGroup({ disableClusteringAtZoom: 15 });
             map.removeLayer(getLayer());
             markers.addLayer(all_sensors);
@@ -574,7 +293,7 @@ function ajax(params) {
             var options = {
                 // Bounding box for all of PEI
                 //bbox: [-64.5, 45.9, -62, 47.1]
-                
+
                 // Bouding box for only Charlottetown
                 bbox: [-63.2, 46.22, -63.08, 46.32]
             };
@@ -632,4 +351,3 @@ $(function() {
 
     });
 });
->>>>>>> gh-pages
